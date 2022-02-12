54ae2bdca106856e1f70bdb014f57d297f4d2c93
NiloBaig <nilo.baig92@gmail.com>
2022-02-12 22:02:40 +0100

    Minor changes

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -5,25 +5,29 @@ import csv
 import datetime
 import matplotlib.pyplot as plt
 
-#A funtion to add your monthly income. Its important to add income the first time you run the program, so that you have enough data to visualize
+
+# A function to add your monthly income. Its important to add income the first time you run the program, so that you
+# have enough data to visualize
 def add_money():
     deposit = int(input("Enter the amount you wish to deposit: "))
     global income
     income += deposit
     print("Income added: ", income)
 
-#This csv file contains expenses with columns: date of the month, expense, and its amount
-expense_sheet='data.csv'
 
-#A function to manually add your expenses randomly
+# This csv file contains expenses with columns: date of the month, expense, and its amount
+expense_sheet = 'data.csv'
+
+
+# A function to manually add your expenses randomly
 def enter_data():
-    #If the csv file does not exist, it will create a new one with the values given by the user
+    # If the csv file does not exist, it will create a new one with the values given by the user
     if not os.path.exists(expense_sheet):
         with open(expense_sheet, 'w', newline="") as file:
             writer = csv.writer(file)
             writer.writerow(['Date', 'Label', 'Expense'])
 
-    #If the csv file exists, it simply appends the additional expense given by user
+    # If the csv file exists, it simply appends the additional expense given by user
     if os.path.exists(expense_sheet):
         read_file_list = list(csv.reader(open(expense_sheet, 'r')))
         if len(read_file_list) == 0:
@@ -31,7 +35,7 @@ def enter_data():
                 writer = csv.writer(file)
                 writer.writerow(['Date', 'Label', 'Expense'])
 
-    #Three values are needed to enter expenses
+    # Three values are needed to enter expenses
     date = int(input("Enter the date (dd): "))
     label = input("Label your Expense: ")
     expense = int(input("Expense amount:"))
@@ -40,7 +44,8 @@ def enter_data():
         writer = csv.writer(file)
         writer.writerow([date, label, expense])
 
-#A function to select what user wish to do
+
+# A function to select what user wish to do
 def choices():
     print("0 : Add income to the account.")
     print("1 : Enter the expenses.")
@@ -52,13 +57,13 @@ def choices():
         choice = int(input())
     except Exception as e:
         print("!Enter a valid Value..", e)
-
     return choice
 
 
-invalid="It looks like you don't have enough data yet to visualize."
+invalid = "It looks like you don't have enough data yet to visualize."
 
-#A function to view all your expenses, balance left, and amount of money you have spent in text
+
+# A function to view all your expenses, balance left, and amount of money you have spent in text
 def data_insights():
     try:
         with open(expense_sheet, 'r') as file:
@@ -82,7 +87,8 @@ def data_insights():
     except Exception as e:
         print(invalid, e)
 
-#A function to view all your expenses in a month in a graphical manner
+
+# A function to view all your expenses in a month in a graphical manner
 def data_visualization():
     try:
         with open(expense_sheet, 'r') as file:
@@ -106,7 +112,8 @@ def data_visualization():
     except Exception as e:
         print(invalid, e)
 
-#A function to view all your expenses in a graphical manner for a given date range
+
+# A function to view all your expenses in a graphical manner for a given date range
 def limited_visualization(start_date, end_date):
     try:
         with open(expense_sheet, 'r') as file:
@@ -155,8 +162,8 @@ if __name__ == "__main__":
             data_visualization()
         elif choice == 4:
             print("Specify:")
-            start = int(input("Start Date: "))
-            end = int(input("End Date: "))
+            start = int(input("Start Date (dd): "))
+            end = int(input("End Date (dd): "))
             limited_visualization(start, end)
         else:
             print("!!INVALID choice.")
