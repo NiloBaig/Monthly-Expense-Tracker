f8b6f1571dbeeaf0cebb4d3b4eae30d6633944f4
NiloBaig <99265854+NiloBaig@users.noreply.github.com>
2022-02-13 15:47:47 +0100

    Gradle Demo

---
+++ gradle-demo/src/main/java/com/nilo/GradleDemo.java
@@ -0,0 +1,7 @@
+package com.nilo;
+
+public class GradleDemo {
+	public static void main(String[] args) {
+		System.out.println("Hello! This is Gradle Demo!");
+	}
+}
---
+++ gradle-demo/src/test/java/com/nilo/GradleDemoTest.java
@@ -0,0 +1,11 @@
+package com.nilo;
+
+import org.junit.Test;
+
+public class GradleDemoTest {
+	
+	@Test
+	public void verifyNoExceptionThrown() {
+		GradleDemo.main(new String[]{});
+	}
+}
